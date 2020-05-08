146e3f38138a24defd9fd9cc223522a6c89c1655
Muhammad_Sameer <muhammadsameer.css@gmail.com>
2020-05-08 21:47:40 +0500

    Herer is the code of tutorial 10
    Don't Forget to Subscribe and like the video

---
+++ tut10.py
@@ -0,0 +1,23 @@
+# names = ['haroon', 'emad', 'salman' , 'subhan', 'amir'] #string list
+# names = ['haroon', 'emad', 'salman' , 'subhan', 'amir',45, 548 , 513] #string + integer list
+
+# integers = [5,1,8,91,84]
+# integers.sort()
+# integers.reverse()
+# integers.insert(3,45)
+# integers.append('IT EDUCATION')
+# integers.pop()
+# integers.remove(91)
+
+
+# numbers = []
+# numbers.append(21)
+# numbers.append(45)
+# numbers.append(18)
+# numbers.append(17)
+# print(numbers)
+
+integer = [5,1,8,91,84]
+
+integer[2] = "SUBSCRIBE NOW"
+print(integer)
