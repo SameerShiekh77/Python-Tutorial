844e56ea77d2313eb6aaca9edf065a086f2ff4d6
Muhammad_Sameer <muhammadsameer.css@gmail.com>
2020-05-17 21:31:58 +0500

    Here is the code of Tutorial 6

---
+++ tut6.py
@@ -0,0 +1,23 @@
+# title = "i am Good Person"
+# print(title)
+# print(title.lower())
+# a = title.upper()
+# print(a)
+# print(title.capitalize())
+# print(title.endswith("Good"))
+# print(title.find("Good"))
+# print(title.replace("Good","bad"))
+# print(title.isalnum())
+# print(title.isalpha())
+
+
+# num = 1,2,4,98,6,8,5
+# print(sum(num))
+
+
+# num1 = -952039641
+# print(abs(num1))
+
+# a = 2*2*2*2*2*2
+# print(a)
+print(pow(5,10))
