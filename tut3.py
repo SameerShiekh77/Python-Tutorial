5eeccecea6c813723388e43726a60a5ae2f16175
Muhammad_Sameer <muhammadsameer.css@gmail.com>
2020-05-17 21:29:21 +0500

    Here is the code of Tutorial 3

---
+++ tut3.py
@@ -0,0 +1,22 @@
+#bhai ye mayra channel hai is ko subscribe kro q k is may python sikhai ja rhi hai 
+"""yr is
+bt ko share\'
+skdksdjf
+fjsadklfj
+fdsakjfklsf
+dfsadkjfadksfioef
+# kro"""
+# print("subscribe IT EDUCATION", end=" ")
+# print("bhai video ko share kro")
+
+# print("HELLO Friends chai peelo\ncomedy\t\tPYTHON is interpreter language")
+
+print("jo bhi print k andr hai bhai us ko mujhy print kr k de do mtlb dheka do ")
+print("back slash = \\")
+
+"""
+Quick Quiz
+print your name,address, gender & nationality with only use one print function
+every statement print in new line   
+"""
+print("name: IT eduction\n")
