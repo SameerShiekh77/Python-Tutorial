939934cf6117b45054a8e146c1ce9cd371e22586
Muhammad_Sameer <muhammadsameer.css@gmail.com>
2020-05-17 21:31:16 +0500

    Here is the code of Tutorial 5

---
+++ tut5.py
@@ -0,0 +1,11 @@
+title = "You are a Programmer"
+# print(len(title))
+# print(title)
+# print(title[0:45]) #first_number = start,  second_number = end
+# print(title[:45])
+# print(title[0:])
+# print(title[:]) ctrl + ?
+# print(title[5:9]) #First_number is including second_number is excluding
+# print(title[9:14])
+print(title[4:8:2]) #third_number = step
+print(title[5:18:3])
