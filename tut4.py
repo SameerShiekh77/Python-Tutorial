9985c6679d7f2be175639374b78b8786bfe42847
Muhammad_Sameer <muhammadsameer.css@gmail.com>
2020-05-17 21:30:33 +0500

    Here is the code of Tutorial 4

---
+++ tut4.py
@@ -0,0 +1,24 @@
+# # num = 45
+# # print(type(num))
+
+# # name = 'It Education'
+# # print(type(name))
+
+# # floating = 879.548
+# # print(type(floating))
+
+# num1 = 87
+# num2 = 45
+# var = "Youtube"  
+# print(num1+ float(num2))
+# num3 = 548.48
+# print(int(num3))
+
+
+print("What is your name?")
+a = input()
+print('Your name is', a)
+
+print("What is your age?")
+b = int(input())
+print('Your age is', b)
