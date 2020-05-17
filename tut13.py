ee04fbab78fd08af73aa5cf72addf199310d49fe
Muhammad_Sameer <muhammadsameer.css@gmail.com>
2020-05-17 21:28:04 +0500

    Here is the code of Exercise 2

---
+++ tut13.py
@@ -0,0 +1,10 @@
+# print('\t\t\t\t\t\tAGE DICTIONARY\nWe have four persons names\n1. Sameer\n2. Emad\n3. Subhan\n4. Saim\n5. Azhar')
+# names = input('Bhai yaha  pr kisi ka name bhi enter kro or us ki age malom kro\n')
+# ageDictionary = {'oooohhh' : 'hi', 'hehehheheheh' : 'hello', 'Subhan' : 24, 'Saim' : 21, 'Azhar' : 23}
+# print("The age is: ",ageDictionary[names])
+
+# print('\t\t\t\t\t\tAGE DICTIONARY\nWe have four persons names\n1. Sameer\n2. Emad\n3. Subhan\n4. Saim\n5. Azhar')
+names = input('Bhai yaha pr kisi bhi alien ka word likho or us ki  meaning jano\n')
+ageDictionary = {'oohh' : 'hi', 'hehehheheheh' : 'hello', 'Subhan' : 24, 'Saim' : 21, 'Azhar' : 23}
+print("The meaning is: ",ageDictionary[names])
+
