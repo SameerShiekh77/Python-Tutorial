efe2b2c560cf42dbb9906720e074fd6937b95eb3
Muhammad_Sameer <muhammadsameer.css@gmail.com>
2020-05-17 21:32:34 +0500

    Here is the code of Tutorial 7

---
+++ tut7.py
@@ -0,0 +1,58 @@
+# operators in python
+
+#1. Arithmethic operators
+
+# print("7 + 9 is ", 7+9)
+# print("7 - 9 is ", 7-9)
+# print("7 / 9 is ", 7/9)
+# print("7 * 9 is ", 7*9)
+# print("9 % 9 is ", 9%9)
+# print("7 ** 3 is ", 7**3)
+# print("9 // 3 is ", 9//3)
+
+#2. Assingment Operators
+
+# a = 78
+# print(a)
+# a += 5 # a = a + 5 
+# print(a)
+# a %= 5
+# print(a)
+
+#3. Comparison operators
+# a = 5
+# b = 5
+
+# print(a==b)
+# print(a>b)
+# print(a<b)
+# print(a>=b)
+# print(a<=b)
+
+
+#4. Logical Operators
+# a = True
+# b = False
+# print(a and b)
+# print(a or b)
+
+#5. Identity Operators
+# print(a is not b) ctrl + ?
+
+#6. Membership Operators
+
+# list1 = [5,9,5,2,9,189,9,842,8,0,8,]
+# print(52 not in list1)
+
+#7. Bitwise operators
+
+# 0 = 00
+# 1 = 01
+# 2 = 10
+# 3 = 11
+# 00
+# 11
+# --
+# 11 = 3
+# print(0 & 1) #& is equal to and
+print(0 | 3) #| is equal to or
