c94f23c6e37f95526cded4faa0ea03def48fa261
Muhammad_Sameer <muhammadsameer.css@gmail.com>
2020-05-17 21:26:53 +0500

    Here is the code of tutorial 12 about dictionary

---
+++ tut12.py
@@ -0,0 +1,20 @@
+breakfastDict = {'Sameer' : 'Egg', 'Emad' : 'Bread', 'Logan' : 'Paratha'}
+# print(type(breakfastDict))
+
+# print(breakfastDict)
+# print(breakfastDict.keys())
+# print(breakfastDict.items())
+
+# breakfastDict['Zunair'] = 'Bread'
+# breakfastDict['Subhan'] = 'Juice'
+
+# del breakfastDict['Logan']
+
+# breakfastDict2 = breakfastDict.copy()
+# del breakfastDict2['Emad']
+# print(breakfastDict2)
+
+# print(breakfastDict.get('Subhaefn'))
+print(breakfastDict['Sameer'])
+
+
