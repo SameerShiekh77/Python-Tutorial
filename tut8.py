8c8adfe5042c64b578ee5e0fa5731ca75434d0f9
Muhammad_Sameer <muhammadsameer.css@gmail.com>
2020-05-17 21:33:36 +0500

    Here is the code of Tutorial 8

---
+++ tut8.py
@@ -0,0 +1,18 @@
+# num1 = 85
+# num2 = 70
+# num3 = 100
+
+# if num1 > num2 and num1 > num3:
+#     print("The value of num1 is greater than num2 and num3")
+# elif num2 > num1 and num2 > num3:
+#     print("The value of num2 is greater than num1 and num3")
+# else:
+#     print("The value of num3 is greater than num1 and num2")
+print("Enter your string")
+var1 = input()
+# var2 = "Subscribe"
+
+if var1 == "IT education":
+    print("SUBSCRIBE THE CHANNEL and LIKE THE VIDEO") 
+else:
+    print("jaldi s subscribe krlo kahi is youtube ki dunia may bhol na jao")
